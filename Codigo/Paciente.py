89bd6c866e727e7f8e64c400ee29c922f96897d5
alemedinab10 <alejandra.medina.benito@alumnos.upm.es>
2023-05-16 01:04:41 +0200

    UI_contornos funcionando

--- Codigo/Paciente.py
+++ Codigo/Paciente.py
@@ -3,6 +3,7 @@ import pydicom
 import re
 import cv2
 import numpy as np
+import shutil
 
 
 class Paciente:
@@ -13,7 +14,7 @@ class Paciente:
         self.files, self.damagedFiles, self.RTSTRUCT = self.importarDatos()
         self.rois = self.obtener_ROI_RTSTRUCT()
         self.nombres_ROI = self.obtenerNombresROI()
-        self.UI_Contornos = self.importarContornosRTSTRUCT()
+        self.UI_Contornos = self.obtenerUI_Contornos()
 
 
     def importarDatos(self):
@@ -82,7 +83,7 @@ class Paciente:
     def obtener_ROIs_y_posicion_del_Dicom (self, numero_en_archivo_dicom):
         r = []
         pos = []
-        dicom = pydicom.dcmread(f"../PacienteEjemplo/{self.paciente}/CT/DICOM_{str(numero_en_archivo_dicom).zfill(3)}.dcm")
+        dicom = pydicom.dcmread(os.path.join(self.direccionBaseDatos, str(self.paciente),"CT", f"DICOM_{str(numero_en_archivo_dicom).zfill(3)}.dcm"))
         plano = self.obtener_Coordenadas(dicom)[2]
         for clave_externa in self.UI_Contornos:
             for i, cont in enumerate(self.UI_Contornos[clave_externa], 0):
@@ -93,7 +94,7 @@ class Paciente:
 
 
     def obtenerMascaraROIEspecifica(self, ROI, numero_dicom):
-        dicom = pydicom.dcmread(f"../PacienteEjemplo/{self.paciente}/CT/DICOM_{str(numero_dicom).zfill(3)}.dcm")
+        dicom = pydicom.dcmread(os.path.join(self.direccionBaseDatos, str(self.paciente),"CT", f"DICOM_{str(numero_dicom).zfill(3)}.dcm"))
         coordenadas = self.obtener_Coordenadas(dicom)
         x_imagen = coordenadas[0]
         y_imagen = coordenadas[1]
@@ -104,7 +105,7 @@ class Paciente:
             if (ROI == roi):
                 posicion = i
         if (posicion == None):
-            print ("El ROI especificado no se encuentra en el DICOM")
+            print (f"ERROR: obtenerMascaraROIEspecifica(ROI=>{ROI}) El ROI especificado no se encuentra en el DICOM")
             return
         mRois = np.zeros((512, 512), dtype=np.int32)
         contornos = []
@@ -117,6 +118,7 @@ class Paciente:
                 scaled_contornos.append((x, y))
         cv2.drawContours(mRois, [np.array(scaled_contornos, dtype=np.int32)], 0, 1, -1)
         return mRois
+
 
     def obtener_ROI_RTSTRUCT (self):
         r = []
@@ -132,9 +134,9 @@ class Paciente:
             nR.append(ROI_name)
         return nR
 
-    def importarContornosRTSTRUCT(self):
-        UI_Contornos = {}
 
+    def _importarContornosRTSTRUCT(self):
+        UI_Contornos = {}
         # Recorre la lista de 'rois'
         for roi in self.rois:
             contour_data_patron = re.compile(r'Contour Data\s+DS:\s+(.*)').findall(str(roi))
@@ -156,8 +158,155 @@ class Paciente:
                     cont_matrix = np.array(cont_matrix)
                     UI_Contornos[int(self.extraerROIName(roi))].append(cont_matrix)
                     posicion+=1
+        return UI_Contornos
+
+
+    def _procesar_RTSTRUCT_como_txt(self):
+        # Abrir archivo y leer su contenido
+        with open(os.path.join(self.direccionBaseDatos, str(self.paciente), "RTSTRUCT.txt"), encoding='utf-8') as archivo:
+            texto = archivo.read()
+
+        # Eliminar caracteres raros
+        texto_limpio = re.sub('[^A-Za-z0-9().-_,/\n]+', ' ', texto)
+        
+        # Sobrescribir el archivo original con el texto limpio
+        with open(os.path.join(self.direccionBaseDatos, str(self.paciente), "RTSTRUCT.txt"), 'w', encoding='utf-8') as archivo:
+            archivo.write(texto_limpio)
+        
+        # Leer el archivo limpio
+        with open(os.path.join(self.direccionBaseDatos, str(self.paciente), "RTSTRUCT.txt"), 'r') as archivo_limpio:
+            # Buscar la línea que contiene 'ManualOverrideMaxThresholdS'
+            lineas_restantes = archivo_limpio.readlines()
+            indice = lineas_restantes.index(next(linea for linea in lineas_restantes if 'ManualOverrideMaxThresholdS' in linea))
+            # Guardar las líneas restantes en un archivo nuevo
+            with open('archivo_nuevo.txt', 'w') as archivo_nuevo:
+                archivo_nuevo.writelines(lineas_restantes[indice+1:])
+        
+        # Eliminar las líneas que contienen 'ManualOverrideMaxThresholdS' del archivo nuevo
+        with open('archivo_nuevo.txt', "r") as f:
+            lineas = f.readlines()
+        new_lines = [line for line in lineas if 'ManualOverrideMaxThresholdS' not in line]
+        with open('archivo_limpio_sin_lineas.txt', "w") as f:
+            f.writelines(new_lines)
+        os.remove('archivo_nuevo.txt')
+
+        # Separa por ROIS
+        search_string = "0 IS"
+        input_file = "archivo_limpio_sin_lineas.txt"
+        output_folder = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt")
+        os.makedirs(output_folder, exist_ok=True)
+        current_roi_position = 0
+        current_output_file = os.path.join(output_folder, f"{self.nombres_ROI[current_roi_position]}.txt")
+        current_group_text = ""
+        
+        with open(input_file, "r") as f:
+            for i, line in enumerate(f, 1):  # Agrega el argumento "1" al enumerate para empezar en la línea 1
+                if (search_string in line):
+                    if current_group_text:
+                        with open(current_output_file, "w") as f_out:
+                            f_out.write(current_group_text)
+
+                        current_group_text = ""
+                        current_roi_position += 1
+                        current_output_file = os.path.join(output_folder, f"{self.nombres_ROI[current_roi_position]}.txt")
+                else:
+                    current_group_text += line
+        if current_group_text:
+            with open(current_output_file, "w") as f_out:
+                f_out.write(current_group_text)
+
+        # Guardar el ultimo ROI
+        search_string = f"0 IS {str(self.nombres_ROI[len(self.nombres_ROI)-2])}"
+        input_file = "archivo_limpio_sin_lineas.txt"
+        output_file = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt", "tempLast.txt")
+        start_writing = False
+        with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+            for line in f_in:
+                if search_string in line:
+                    start_writing = True
+                    continue
+                if start_writing:
+                    f_out.write(line)
+        with open(output_file, "r") as f, open(os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt", f"{str(self.nombres_ROI[len(self.nombres_ROI)-1])}.txt"), "w") as output:
+            for line in f:
+                if len(line.split("\\")) > 1 and any(char.isdigit() for char in line.split("\\")[1]) and "UI" not in line:
+                    # línea contiene números entre \\ y no contiene "UI", agregar al archivo de salida
+                    output.write(line)
+        os.remove(output_file)
+
+        # Filtrar los saltos de linea no esperados
+        input_dir = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt")
+        output_dir = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt")
+        for filename in os.listdir(input_dir):
+            if filename.endswith(".txt"):
+                in_F = os.path.join(input_dir, filename)
+                out_F = os.path.join(output_dir, f"ROI_{filename}")
+                with open(in_F, "r") as archivo:
+                    lineas = archivo.readlines()
+                with open(out_F, "w") as archivo_modificado:
+                    for linea in lineas:
+                        if re.search(r"0P\s\S*\n", linea):
+                            linea = linea.replace("\n", " ")
+                        archivo_modificado.write(linea)
+                os.remove(in_F)
+
+        # Dejar solo las lineas de los Contours
+        input_dir = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt")
+        output_dir = os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt")
+        for filename in os.listdir(input_dir):
+            if filename.endswith(".txt"):
+                input_file = os.path.join(input_dir, filename)
+                output_file = os.path.join(output_dir, f"f{filename}")
+                with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+                    filtered_lines = filter(lambda line: line.find("CLOSED_PLANAR") != -1, f_in)
+                    f_out.writelines(filtered_lines)
+                with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
+                    for line in f_in:
+                        words = line.split()
+                        try:
+                            index_0p = words.index("0P")
+                            new_line = " ".join(words[index_0p+2:])
+                        except ValueError:
+                            new_line = line
+                        f_out.write(new_line)
+                os.remove(input_file)
+        os.remove("archivo_limpio_sin_lineas.txt")
+
+
+    def obtenerUI_Contornos(self):
+        UI_Contornos = self._importarContornosRTSTRUCT()
+        self._procesar_RTSTRUCT_como_txt()
+
+        for clave_externa in UI_Contornos:
+            with open(os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt", f"fROI_{clave_externa}.txt"), "r") as archivo:
+                lineas = archivo.readlines()
+
+            for i, cont in enumerate(UI_Contornos[clave_externa], 0):
+                if (len(cont) == 1):
+                    try:
+                        numeros = re.findall(r'\d+(?:\.\d+)?', lineas[i][:lineas[i].index("0 SQ")])
+                    except ValueError:
+                        numeros = re.findall(r'\d{1,3}(?:\.\d{1,3})?', lineas[i])
+                    except IndexError:
+                        pass
+
+                    
+                    numeros = np.array(numeros)
+                    numeros = np.negative(numeros.astype(np.float64))
+                    n_pad = 3 - len(numeros) % 3
+                    if n_pad < 3:
+                        numeros = np.pad(numeros, (0, n_pad), mode='constant', constant_values=0)
+                        
+                    numeros = numeros.reshape(-1, 3)
+
+                    UI_Contornos[clave_externa][i] = numeros
+
+        # Borro los archivos de texto sobrantes
+        shutil.rmtree(os.path.join(self.direccionBaseDatos, str(self.paciente), "ROI_txt"))
 
         return UI_Contornos
+
+
 
 o = Paciente(29)
 
